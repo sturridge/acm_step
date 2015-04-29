8d19f82ce336ff15f15af7ae300f43e57ad90b2d
qinshiheng <qinshiheng@yeah.net>
2015-04-29 10:47:18 +0800

    unfinished

--- section 2/1.2.1.cpp
+++ section 2/1.2.1.cpp
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main(){
-	int n = u = d = 0;
+	int n = 0, u = 0, d = 0, t = 0;
 	while(1){
 		~scanf("%d %d %d", &n, &u, &d);
-		if (n == 0 && u == 0 && d == 0){
-			
+		if (n == 0 && u == 0 && d == 0)
+			return 0;
+		while(n > 0){
+			n -= u;
+			t++;
+			if (n <= 0)
+				printf("%d\n", t);
+			t++; // rest
+			n += d;
+			t++;
 		}
+		t = 0;
 	}
 }
